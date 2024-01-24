2feff3970371fd4dd5822acad9f1beab8c7f4783
Avadhut Lohar <loharavdhoot@gmail.com>
2024-01-24 21:07:38 +0530

    Login and Signup form Created

---
+++ src/components/Login.jsx
@@ -0,0 +1,60 @@
+import React from "react";
+import Header from "./Header";
+import { useState } from "react";
+
+const Login = () => {
+  const [isSignInForm, setIsSignInForm] = useState(true);
+
+  const toggleSignInForm = () => {
+    setIsSignInForm(!isSignInForm);
+  };
+
+  return (
+    <div className="relative min-h-screen">
+      <Header />
+      <div className="absolute inset-0 overflow-hidden">
+        <img
+          className="w-full h-full object-cover"
+          src="https://assets.nflxext.com/ffe/siteui/vlv3/16006346-87f9-4226-bc25-a1fb346a2b0c/9662d0fd-0547-4665-b887-771617268815/IN-en-20240115-popsignuptwoweeks-perspective_alpha_website_large.jpg"
+          alt="Netflix Background"
+        />
+      </div>
+
+      <div className="absolute inset-0 flex items-center justify-center">
+        <form className="sm:w-3/5 md:w-2/5 lg:w-1/3 xl:w-1/4 absolute p-8 bg-black my-20 mx-auto text-white rounded-lg bg-opacity-80">
+          <h1 className="font-bold text-3xl mb-6">
+            {isSignInForm ? "Sign In" : "Sign Up"}
+          </h1>
+
+          {!isSignInForm && (
+            <input
+              type="text"
+              placeholder="Full Name"
+              className="p-4 mb-4 w-full bg-gray-700 rounded-md"
+            />
+          )}
+          <input
+            type="text"
+            placeholder="Email Address"
+            className="p-4 mb-4 w-full bg-gray-700 rounded-md"
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            className="p-4 mb-4 w-full bg-gray-700 rounded-md"
+          />
+          <button className="p-4 mb-6 bg-red-700 w-full rounded-lg">
+            {isSignInForm ? "Sign In" : "Sign Up"}
+          </button>
+          <p className="text-sm" onClick={toggleSignInForm}>
+            {isSignInForm
+              ? "New to Netflix? Sign Up Now"
+              : "Already registered? Sign In Now."}
+          </p>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
