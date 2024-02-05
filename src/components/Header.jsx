5a88345dbe4aa8abce7644dd38b4be051d0fc955
Avadhut Lohar <loharavdhoot@gmail.com>
2024-02-05 19:59:28 +0530

    unsubscribe onAuthStateChanged callback

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,19 +8,17 @@ import { addUser, removeUser } from "../utils/userSlice";
 const Header = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
-  const user = useSelector((store => store.user));
+  const user = useSelector((store) => store.user);
   const handleSignOut = () => {
     signOut(auth)
-      .then(() => {
-       
-      })
+      .then(() => {})
       .catch((error) => {
         navigate("/error");
       });
   };
 
   useEffect(() => {
-    onAuthStateChanged(auth, (user) => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
       if (user) {
         const { uid, email, displayName, photoURL } = user;
         dispatch(
@@ -30,13 +28,17 @@ const Header = () => {
             displayName: displayName,
             photoURL: photoURL,
           })
-        ); 
-        navigate("/browse")
+        );
+        navigate("/browse");
       } else {
         dispatch(removeUser());
         navigate("/");
       }
     });
+
+    return () => {
+      unsubscribe();
+    };
   }, []);
 
   return (
@@ -46,16 +48,18 @@ const Header = () => {
         src="https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png"
         alt="logo"
       />
-     {user && <div className="flex p-2 ">
-        <img
-          className="w-12 h-12 rounded-lg"
-          src={user.photoURL}
-          alt="usericon"
-        />
-        <button onClick={handleSignOut} className="font-bold text-white">
-          Sign out
-        </button>
-      </div>}
+      {user && (
+        <div className="flex p-2 ">
+          <img
+            className="w-12 h-12 rounded-lg"
+            src={user.photoURL}
+            alt="usericon"
+          />
+          <button onClick={handleSignOut} className="font-bold text-white">
+            Sign out
+          </button>
+        </div>
+      )}
     </div>
   );
 };
